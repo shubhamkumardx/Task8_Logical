58375dd8fdde6dfe4d820dd299c6a0fa54ef9978
shubhamkumardx <shubhamkumar1.dx@gmail.com>
2023-11-14 16:10:13 +0530

    third commit

--- src/Components/New_comp.jsx
+++ src/Components/New_comp.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
+
 function New_comp(props) {
-    const [minutes, setMinutes] = useState(+window.localStorage.getItem("count"));
-    const [seconds, setSeconds] = useState(+window.localStorage.getItem("cot"));
+    const [minutes, setMinutes] = useState(sessionStorage.getItem("minutes") ?? 0);
+    const [seconds, setSeconds] = useState(sessionStorage.getItem("seconds") ?? 0);
     const [flag, setFlag] = useState(false);
 
 
@@ -10,12 +11,10 @@ function New_comp(props) {
         setMinutes(e.target.value);
     }
 
-
     const handlesecond = (e) => {
         setSeconds(e.target.value)
     }
-
- 
+    console.log(minutes, typeof (sessionStorage.getItem("minutes")), parseInt(sessionStorage.getItem("minutes")));
     useEffect(() => {
         if (flag) {
             const interval = setInterval(() => {
@@ -23,29 +22,28 @@ function New_comp(props) {
                     setSeconds((seconds) => seconds + 59);
                     setMinutes((minutes) => minutes - 1);
                 } else if (parseInt(seconds) == 0 && parseInt(minutes) == 0) {
+
                 } else {
                     setSeconds((seconds) => seconds - 1);
                 }
             }, 1000);
             return () => {
                 clearInterval(interval);
-                console.log(interval)
             };
         }
     }, [seconds, minutes, flag]);
 
 
+
     const startt = () => {
-        setFlag(true);
-        setMinutes(window.localStorage.getItem("count"))
-        setSeconds(window.localStorage.getItem("cot"))
-    }
+        setFlag(true)
+    };
 
     const stop = () => {
         setFlag(false);
-        setMinutes(window.localStorage.setItem("count", minutes));
-        setSeconds(window.localStorage.setItem("cot", seconds));
-    }
+        sessionStorage.setItem("minutes", minutes);
+        sessionStorage.setItem("seconds", seconds);
+    };
 
 
     return (
@@ -64,29 +62,32 @@ function New_comp(props) {
                         <input type='text' onChange={handlesecond} className='cs-form4' name='second' />
                         <p className='cs-form1'>second</p>
                     </div>
-                    {
-                    flag ?
-                        <p className='cs-form3 text-center fw-bold'>{parseInt(minutes) < 10 ? "0" + minutes : minutes}:{parseInt(seconds) < 10 ? "0" + seconds : seconds}</p>
-                        :
-                        <p className='cs-form3 text-center fw-bold'>{parseInt(localStorage.getItem("count")) < 10 ? "0" + parseInt(localStorage.getItem("count")) : parseInt(localStorage.getItem("count"))}
-                            :{parseInt(localStorage.getItem("cot")) < 10 ? "0" + parseInt(localStorage.getItem("cot")) : parseInt(localStorage.getItem("cot"))}</p>
-                    }
+                    {/* {
+                        flag ?
+                            <p className='cs-form3 text-center fw-bold'>{parseInt(minutes) < 10 ? "0" + minutes : minutes}:{parseInt(seconds) < 10 ? "0" + seconds : seconds}</p>
+                            :
+                            <p className='cs-form3 text-center fw-bold'>{parseInt(sessionStorage.getItem("minutes")) < 10 ? "0" + parseInt(sessionStorage.getItem("minutes")) : parseInt(sessionStorage.getItem("minutes"))}
+                                :{parseInt(sessionStorage.getItem("seconds")) < 10 ? "0" + parseInt(sessionStorage.getItem("seconds")) : parseInt(sessionStorage.getItem("seconds"))}</p>
+                    } */}
+
+                    <p className='cs-form3 text-center fw-bold'>{parseInt(minutes) < 10 ? "0" + minutes : minutes}:{parseInt(seconds) < 10 ? "0" + seconds : seconds}</p>
 
 
                     <div className='text-center mt-5'>
+
                         {
-                            flag ?
-                                <button type="button" className="btn btn-danger  text-white" onClick={stop}>Stop</button>
-                                :
-                                <button type="button" className="btn btn-success" onClick={startt}>Start</button>
+                            flag ? 
+                            <button type="button" className="btn btn-danger  text-white" onClick={stop}>Stop</button>
+                            :
+                          
+                            <button type="button" className="btn btn-success" onClick={startt}>Start</button>
                         }
-
-
+                      
+                     
                     </div>
 
                 </div>
             </div>
-
 
         </div>
     );
